7d86a23032f8cf027f65a01319b41c65eab76cae
tungpg <pgtung@gmail.com>
2023-04-16 11:35:49 +0700

    support awx

--- assets/js/checkout_hook.js
+++ assets/js/checkout_hook.js
@@ -113,7 +113,7 @@ jQuery(function ($) {
 			}
         }
         if ((typeof event.data === 'object') && event.data.name === 'infipay-paymentIntentIdAirwallex') {
-	alert(JSON.stringify(data));
+	alert(JSON.stringify(event.data));
             var paymentIntentId = event.data.value.paymentIntentId;
             
             if (infipay_checkout_form.find('[name="infipay-awx-payment-intent-id"]')) {
